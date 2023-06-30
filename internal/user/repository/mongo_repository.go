2e2bfdfc0c0c3438dd64f868f0b3e537f986d605
Kiryl Matsenka <kiryl.matysenka@wrenkitchens.com>
2023-07-01 00:17:58 +0200

    mongo repository

---
+++ internal/user/repository/mongo_repository.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"context"
+	"secret-keeper/internal/user/domain"
+
+	"github.com/KirylJazzSax/secret-keeper/internal/user/domain"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type MongoUserRepository struct {
+	client *mongo.Client
+}
+
+func (r *MongoUserRepository) CreateUser(ctx context.Context, u *domain.User) error {
+	coll := r.client.Database("secret-keeper").Collection("users")
+	_, err := coll.InsertOne(ctx, u)
+	return err
+}
+
+func (r *MongoUserRepository) GetUser(ctx context.Context, email string) (*domain.User, error) {
+	coll := r.client.Database("secret-keeper").Collection("users")
+	filter := bson.D{{"email", email}}
+	var user *domain.User
+
+	err := coll.FindOne(ctx, filter).Decode(user)
+
+	return user, err
+}
+
+func NewMongoUserRepository(client *mongo.Client) *MongoUserRepository {
+	return &MongoUserRepository{
+		client: client,
+	}
+}
