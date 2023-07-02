4841b17a6c1297b0f1ba74bf0b96d8d5f3d90c02
Kiryl Matsenka <kiryl.matysenka@wrenkitchens.com>
2023-07-02 16:27:05 +0200

    update user module

--- internal/user/repository/mongo_repository.go
+++ internal/user/repository/mongo_repository.go
@@ -2,7 +2,6 @@ package repository
 
 import (
 	"context"
-	"secret-keeper/internal/user/domain"
 
 	"github.com/KirylJazzSax/secret-keeper/internal/common/db"
 	"github.com/KirylJazzSax/secret-keeper/internal/common/errors"
