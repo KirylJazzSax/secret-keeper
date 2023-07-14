24e9653c8bcf9196e88c0b5f10087ad5de75aff4
Kiryl Matsenka <kiryl.matysenka@wrenkitchens.com>
2023-07-15 00:35:36 +0200

    update type

--- internal/secret/server/server.go
+++ internal/secret/server/server.go
@@ -60,7 +60,7 @@ func (s *Server) SecretsList(ctx context.Context, r *secret.SecretsListRequest) 
 	resSecrets := make([]*secret.Secret, len(secrets))
 	for i, s := range secrets {
 		resSecrets[i] = &secret.Secret{
-			Id:    s.Id,
+			Id:    s.Id.String(),
 			Title: s.Title,
 			Body:  s.Body,
 		}
