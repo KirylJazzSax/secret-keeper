e1149ac38191614826dabbc0363b96a49da56ebe
Kiryl Matsenka <kiryl.matysenka@wrenkitchens.com>
2023-07-22 23:44:59 +0200

    update logs

--- internal/common/logs/logs.go
+++ internal/common/logs/logs.go
@@ -6,20 +6,19 @@ import (
 
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
 	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 )
 
 func InterceptorLogger(l zerolog.Logger) logging.Logger {
 	return logging.LoggerFunc(func(_ context.Context, lvl logging.Level, msg string, fields ...any) {
 		switch lvl {
 		case logging.LevelDebug:
-			_ = log.Debug().Msg(msg)
+			_ = l.Debug().Msg(msg)
 		case logging.LevelInfo:
-			_ = log.Info().Msg(msg)
+			_ = l.Info().Msg(msg)
 		case logging.LevelWarn:
-			_ = log.Warn().Msg(msg)
+			_ = l.Warn().Msg(msg)
 		case logging.LevelError:
-			_ = log.Error().Msg(msg)
+			_ = l.Error().Msg(msg)
 		default:
 			panic(fmt.Sprintf("unknown level %v", lvl))
 		}
