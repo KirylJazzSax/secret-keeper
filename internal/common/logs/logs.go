0fbf7023682ac6bc09d10ca85f58789e7115729b
Kiryl Matsenka <kiryl.matysenka@wrenkitchens.com>
2023-07-22 23:53:45 +0200

    update uptade

--- internal/common/logs/logs.go
+++ internal/common/logs/logs.go
@@ -12,13 +12,13 @@ func InterceptorLogger(l zerolog.Logger) logging.Logger {
 	return logging.LoggerFunc(func(_ context.Context, lvl logging.Level, msg string, fields ...any) {
 		switch lvl {
 		case logging.LevelDebug:
-			_ = l.Debug().Str("msg", msg).Msg("")
+			l.Debug().Str("msg", msg).Msg("")
 		case logging.LevelInfo:
-			_ = l.Info().Str("msg", msg).Msg("")
+			l.Info().Str("msg", msg).Msg("")
 		case logging.LevelWarn:
-			_ = l.Warn().Str("msg", msg).Msg("")
+			l.Warn().Str("msg", msg).Msg("")
 		case logging.LevelError:
-			_ = l.Error().Str("msg", msg).Msg("")
+			l.Error().Str("msg", msg).Msg("")
 		default:
 			panic(fmt.Sprintf("unknown level %v", lvl))
 		}
